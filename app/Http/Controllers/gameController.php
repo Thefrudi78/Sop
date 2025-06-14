b6a4cf21012d5eb55199f05c83de30fc7fdb2d6d
Thefrudi78 <thefrudi78@gmail.com>
2025-06-15 02:01:27 +0700

    Make the characters images shows their name.

--- app/Http/Controllers/gameController.php
+++ app/Http/Controllers/gameController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\character;
-
+use App\Models\game;
 use Illuminate\Http\Request;
 
 class gameController extends Controller
@@ -10,6 +10,7 @@ class gameController extends Controller
     public function index($id)
     {   
         $characters = character::where('game_id', $id)->get();
-        return view('index.game', compact('characters'));
+        $game = game::find($id);
+        return view('index.game', compact('characters', 'game'));
     }
 }
--- resources/views/index/game.blade.php
+++ resources/views/index/game.blade.php
@@ -1,9 +1,26 @@
 <x-layout>
-    <div class="grid grid-cols-1 md:grid-cols-4 gap-4 justify-items-center">
+    <div class="flex justify-center pt-10 pb-10">
+        <div class="relative group w-150 h-72">
+            <img
+                src={{ asset($game['image']) }}
+                alt="Sample Image"
+                class="w-full h-full object-cover rounded-xl transition duration-300 group-hover:blur-sm"
+            />
+
+            <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
+                <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Click Me</button>
+            </div>
+        </div>
+    </div>
+    <h3 class="text-left text-2xl font-bold pl-15 mb-4">Characters</h3>
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-4 justify-items-center pt-10">
     @foreach($characters as $character)
-        <div class="w-48 aspect-[3/4] overflow-hidden rounded-2xl shadow-lg">
-            <img src={{ asset($character['image']) }} alt="Portrait Image"
+        <div class="w-48 aspect-[3/4] overflow-hidden rounded-2xl shadow-lg relative group">
+            <img src={{ asset($character['image']) }} alt="{{ $character['name'] }} Image"
                 class="w-full h-full object-cover">
+            <div class="absolute bottom-0 left-0 w-full h-1/3 bg-black opacity-60 flex items-end justify-center translate-y-full group-hover:translate-y-0 transition-transform duration-300">
+                <span class="text-white text-lg font-semibold mb-3">{{ $character['name'] }}</span>
+            </div>
         </div>
     @endforeach
     </div>
